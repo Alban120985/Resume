5e2fb817468fc9f40588992264f9de5ddb9ce488
Alban120985 <39760374+Alban120985@users.noreply.github.com>
2018-09-27 22:19:05 +0200

    Fix:Resolve quadrilateral size

--- src/components/Quadrilateral.vue
+++ src/components/Quadrilateral.vue
@@ -1,10 +1,32 @@
 <template>
-    <div class="quadrilateral"></div>
+    <div class="quadrilateral" v-bind:style="{ 'border-width': quadrilateralSize, left: quadrilateralLeft }" ></div>
 </template>
 
 <script>
 export default {
-  name: "quadrilateral"
+  name: "quadrilateral",
+  data() {
+    return {
+      quadrilateralSize: "0 0",
+      quadrilateralLeft: "0"
+    }
+  },
+  mounted() {
+    this.defineQuadrilateralSize(window.innerHeight, window.innerWidth)
+    //console.log("first width: " + window.innerWidth + " height: " + window.innerHeight)
+    this.$nextTick(() => {
+      window.addEventListener('resize', () => {
+       //console.log("width: " + window.innerWidth + " height: " + window.innerHeight)
+       this.defineQuadrilateralSize(window.innerHeight, window.innerWidth)
+      });
+    })
+  },
+  methods: {
+    defineQuadrilateralSize : function(height, width) {
+      this.quadrilateralSize = (height * .5) + "px 0 0 " + width * 1.5 + "px"
+      this.quadrilateralLeft = "-" + (width * .5) + "px"
+    }
+  }
 };
 </script>
 
@@ -13,10 +35,8 @@ export default {
   width: 0%;
   height: 100%;
   border-style: solid;
-  border-width: 300px 0 0 617px;
   border-color: #006064 transparent;
   position: absolute;
-  left: -160px;
   top: 0;
   z-index: -1;
 }
